b3be955ddd8c30c401c34da4f03d4b7eb97eb345
Arsen Ibatullin <arsen.ibatullin21@gmail.com>
2026-04-30 21:31:28 +0500

    changing the structure of catalog html

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,8 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    path('catalog/', views.CatalogPageView.as_view(), name='catalog'),
-    # path('catalog/<slug:category_slug>', views.CatalogPageView.as_view(), name='catalog_by_filter')
+    path('collection/', views.CatalogPageView.as_view(), name='catalog'),
+    path('catalog/mens/', views.GenderPageView.as_view(gender='men'), name='men_catalog'),
+    path('catalog/women/', views.GenderPageView.as_view(gender='women'), name='women_catalog'),
+    path('detial/<slug:product_slug>', views.ProductDetailPageView.as_view(), name='detail')
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import ListView
 
-from main.models import Product, Category, Size
+from main.models import Product, Category
 
 
 # Create your views here.
@@ -19,62 +20,106 @@ class HomePageView(ListView):
         return context
 
 
-class CatalogPageView(ListView):
+class CatalogFilterMixin:
+    def apply_filters(self, queryset):
+        category = self.request.GET.get('category', '')
+        gender = self.request.GET.get('gender', '')
+        size = self.request.GET.get('size', '')
+        colors = self.request.GET.getlist('color')
+        sort = self.request.GET.get('sort', '')
+
+        if category:
+            queryset = queryset.filter(category__slug=category)
+
+        if gender and not getattr(self, 'gender', None):
+            queryset = queryset.filter(gender=gender)
+
+        if size:
+            queryset = queryset.filter(variants__size__slug=size)
+
+        if colors:
+            queryset = queryset.filter(variants__color__slug__in=colors)
+
+        if sort and sort != 'featured':
+            if sort == 'highrate':
+                queryset = queryset.order_by('-rating')
+            elif sort == 'pricelow':
+                queryset = queryset.order_by('price')
+            elif sort == 'pricehigh':
+                queryset = queryset.order_by('-price')
+            elif sort == 'newtoold':
+                queryset = queryset.order_by('-created_at')
+
+        return queryset.distinct()
+
+
+
+
+class CatalogPageView(CatalogFilterMixin, ListView):
     model = Product
     template_name = 'main/catalog.html'
     context_object_name = 'products'
 
     def get_queryset(self):
-        self.category = None
         queryset = Product.objects.filter(available=True)
+        return self.apply_filters(queryset)
 
-        size_slug = self.request.GET.get('size', '')
-        gender_slug = self.request.GET.get('gender', '')
-        category_slug = self.request.GET.get('category', '')
-        color_slugs = self.request.GET.getlist('color')
-        sort_slug = self.request.GET.get('sort', '')
 
-        category = Category.objects.filter(slug=category_slug).first()
-        size = Size.objects.filter(slug=size_slug).first()
-
-        if category:
-            self.category = category
-            queryset = Product.objects.filter(category=category)
-
-        if size:
-            queryset = queryset.filter(variants__size__slug=size_slug)
-
-        if gender_slug:
-            queryset = queryset.filter(gender=gender_slug)
-
-        if color_slugs:
-            queryset = queryset.filter(variants__color__slug__in=color_slugs)
-
-        if sort_slug and sort_slug != 'featured':
-            if sort_slug == 'highrate':
-                queryset = queryset.order_by('-rating')
-            elif sort_slug == 'pricelow':
-                queryset = queryset.order_by('price')
-            elif sort_slug == 'pricehigh':
-                queryset = queryset.order_by('-price')
-            elif sort_slug == 'newtoold':
-                queryset = queryset.order_by('-created_at')
-
-        return queryset.distinct()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['category'] = self.category
+        category = self.request.GET.get('category', '')
+
+        context['category'] = Category.objects.filter(slug=category).first()
         context['categories'] = Category.objects.all()
         context['selected_size'] = self.request.GET.get('size', '')
         context['selected_gender'] = self.request.GET.get('gender', '')
         context['selected_colors'] = self.request.GET.getlist('color')
         context['selected_sort'] = self.request.GET.get('sort', 'featured')
         context['total_products'] = Product.objects.all().count()
+        context['catalog_clear_url'] = reverse('main:catalog')
+        context['show_gender_filter'] = True
         return context
 
     def get_template_names(self):
         if self.request.headers.get('HX-Request') == 'true':
             return ['main/partials/catalog_partial.html']
         return ['main/catalog.html']
+
+
+
+
+class GenderPageView(CatalogFilterMixin, ListView):
+    model = Product
+    gender = None
+    template_name = 'main/gender_catalog.html'
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        queryset = Product.objects.filter(available=True, gender=self.gender)
+        return self.apply_filters(queryset)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category = self.request.GET.get('category', '')
+
+        context['category'] = Category.objects.filter(slug=category).first()
+        context['categories'] = Category.objects.all()
+        context['selected_size'] = self.request.GET.get('size', '')
+        context['selected_gender'] = self.request.GET.get('gender', '')
+        context['selected_colors'] = self.request.GET.getlist('color')
+        context['selected_sort'] = self.request.GET.get('sort', 'featured')
+
+        context['catalog_gender'] = self.gender
+        context['catalog_title'] = 'Men' if self.gender == 'men' else 'Women'
+        context['total_products'] = Product.objects.filter(available=True, gender=self.gender).count()
+        context['catalog_clear_url'] = reverse('main:men_catalog' if self.gender == 'men' else 'main:women_catalog')
+        context['show_gender_filter'] = False
+        return context
+
+    def get_template_names(self):
+        if self.request.headers.get('HX-Request') == 'true':
+            return ['main/partials/catalog_partial.html']
+        return ['main/gender_catalog.html']
+
 
